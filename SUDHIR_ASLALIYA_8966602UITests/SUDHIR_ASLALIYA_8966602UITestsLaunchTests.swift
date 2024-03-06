16b395a812ded9ae298c3f8172df9a7d94fcc348
sudhir <sudhiraslaiya77@gmail.com>
2024-03-06 11:14:42 -0800

    Initial Commit

---
+++ SUDHIR_ASLALIYA_8966602UITests/SUDHIR_ASLALIYA_8966602UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SUDHIR_ASLALIYA_8966602UITestsLaunchTests.swift
+//  SUDHIR_ASLALIYA_8966602UITests
+//
+//  Created by user240106 on 3/6/24.
+//
+
+import XCTest
+
+final class SUDHIR_ASLALIYA_8966602UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
